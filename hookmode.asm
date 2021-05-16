ed3610d75cba720b72c2b4ae4edfd99390107edd
NotExactlySiev <sadrabp@outlook.com>
2021-05-16 19:31:00 +0430

    smoother circular motion

--- hookmode.asm
+++ hookmode.asm
@@ -10,60 +10,100 @@
         sta angle0
 
 
-              
+        
         lda angle0
         sta $23
         clc
         adc #$40
-        sta $25		; v and a can be used as temporary var here
+        sta $26	
+        lda angle1
+        sta $24
+        sta $27
+        lda #0
+        sta $25
+        sta $28
+	; v and a can be used as temporary var here
 	
-        ldx #2
+        ldx #3
 
 SinCos:			; look up angles for sin and cos with sign bit after
         ldy #0
         lda $23,x
         bpl .check1
+        
+        lda $24,x
         eor #$ff
         clc
         adc #1
-        ldy #1
-.check1	
+        sta $24,x
+        lda $23,x
+        eor #$ff
+        adc #0
+        sta $23,x
+        
+        lda #1
+        sta $25,x
+        
+.check1	lda $23,x
 	asl
 	bpl .check2
+        
+        lda $24,x
         eor #$ff
         clc
-        adc #2
-.check2	lsr
-	sta $23,x
-        sty $24,x
-
+        adc #1
+        sta $24,x
         lda $23,x
+        eor #$ff
+        adc #0
+        sta $23,x
+.check2
+
+	lda $23,x
         cmp #$40
         bne .nright
-	lda radius
-        sta relpx0,x
-        lda #0
-        sta relpx1,x
-	jmp .lookupover
-.nright
-	clc
-        asl
-        sta sinptr
         
-	lda radius
-	lsr
+        lda radius
+        sta relpx0,x
+        jmp .lookupover
+        
+.nright
+
+        lda radius
         clc
         adc #$b0
         sta sinptr+1
+
+	clc
+        lda $24,x
+        adc #$20
+        sta $24,x
         lda $23,x
-        and #$1
+        adc #0
+        sta $23,x
+        
         clc
-        ror
-        ror
-        clc
-        adc sinptr
+        asl
+        asl
+        asl
+        bcc .nbig
+	inc sinptr+1
+.nbig
 	sta sinptr
-	
+        
+	lda $24,x
+        rol
+        rol
+        rol
+        rol
+        and #$06
+        ora sinptr
+        sta sinptr
+        jmp .midwayover
+
+.midway	bpl SinCos        
+.midwayover
+
         ldy #0
         lda (sinptr),y
         sta relpx0,x
@@ -73,7 +113,7 @@ SinCos:			; look up angles for sin and cos with sign bit after
 
 .lookupover
 	       
-        lda $24,x
+        lda $25,x
         beq .positive
 	clc
         lda relpx1,x
@@ -84,12 +124,12 @@ SinCos:			; look up angles for sin and cos with sign bit after
         eor #$ff
         adc #0
         sta relpx0,x
-         
-        
+                
 .positive        
         dex
         dex
-        bpl SinCos
+        dex
+        bpl .midway
 
 
 
